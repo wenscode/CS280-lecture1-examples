ae1c2bfb9e1b2277b91ff17db79aaccf2eb7a53c
wenscode <wenschair@gmail.com>
2017-03-03 20:34:24 +0800

    初始化專案

---
+++ CS280-lecture1-examples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_lecture1_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
